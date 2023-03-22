fe43dbe218b3bed8a9472d03a6d377ae8d396869
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2023-03-23 01:00:13 +0800

    Update entrypoint.sh

--- arm64v8-debian-11/entrypoint.sh
+++ arm64v8-debian-11/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /home/container
+# Output Current Box86/Box64 Version
+box64 -version
+box86 -version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
