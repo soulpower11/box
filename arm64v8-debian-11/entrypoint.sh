9d0e19af142c87118be988f7b391acb43ec2d4d4
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2023-03-23 09:23:34 +0800

    update

--- arm64v8-debian-11/entrypoint.sh
+++ arm64v8-debian-11/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 cd /home/container
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# set this variable, dotnet needs it even without it it reports to `dotnet --info` it can not start any aplication without this
+export DOTNET_ROOT=/usr/share/
+
+# print the dotnet version on startup
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mdotnet --version\n"
+dotnet --version
+
 # Output Current Box86/Box64 Version
 box64 --version
 box86 --version
